a06e960704c3eefba68f46f85e7c456167c4f757
James Unicomb <jamesunicomb@Jamess-MacBook-Pro.local>
2017-10-19 01:35:33 +1100

    update

--- CartPole/q_learning.py
+++ CartPole/q_learning.py
@@ -11,51 +11,45 @@ from theano.tensor.extra_ops import to_one_hot
 from theano.tensor.raw_random import multinomial
 
 import lasagne
-from lasagne.updates import adam
+from lasagne.updates import adam, norm_constraint
 from lasagne.objectives import squared_error
 from lasagne.layers import DenseLayer, InputLayer, ConcatLayer, get_output, \
                            get_all_params, get_all_param_values, set_all_param_values
 from lasagne.nonlinearities import rectify, softmax, tanh
 from lasagne.init import Constant, Normal
 
-n_input       = 4
-n_output      = 2
+n_state       = 4
+n_action      = 2
 learning_rate = 0.001
 learning_tau  = 0.001
-gamma         = 0.95
+gamma         = 0.99
 
 eps_max   = 1.00
 eps_min   = 0.01
-eps_decay = 0.995
+eps_decay = 0.9975
 
 n_epochs    = 1000
-batch_size  = 256
-buffer_size = 10000
+batch_size  = 32
+buffer_size = 40000
 
 def q_network(state):
     input_state = InputLayer(input_var = state,
-                             shape     = (None, n_input))
+                             shape     = (None, n_state))
 
     dense_1     = DenseLayer(input_state,
-                             num_units    = n_input,
+                             num_units    = n_state,
                              nonlinearity = tanh,
                              W         = Normal(0.1, 0.0),
                              b         = Constant(0.0))
 
     dense_2     = DenseLayer(dense_1,
-                             num_units    = n_input,
+                             num_units    = n_state,
                              nonlinearity = tanh,
                              W         = Normal(0.1, 0.0),
                              b         = Constant(0.0))
 
-    dense_3     = DenseLayer(dense_1,
-                             num_units    = n_input,
-                             nonlinearity = tanh,
-                             W         = Normal(0.1, 0.0),
-                             b         = Constant(0.0))
-
-    q_values    = DenseLayer(dense_3,
-                             num_units    = n_output,
+    q_values    = DenseLayer(dense_2,
+                             num_units    = n_action,
                              nonlinearity = None,
                              W         = Normal(0.1, 0.0),
                              b         = Constant(0.0))
@@ -68,7 +62,7 @@ X_action         = T.bvector()
 X_reward         = T.fvector()
 X_done           = T.bvector()
 
-X_action_hot = to_one_hot(X_action, n_output)
+X_action_hot = to_one_hot(X_action, n_action)
 
 q_        = q_network(X_state);      q        = get_output(q_)
 q_target_ = q_network(X_next_state); q_target = get_output(q_target_)
@@ -100,7 +94,7 @@ update_network = theano.function(inputs               = [X_state,
 def get_action(state, step):
     eps = max(eps_min, eps_min + (eps_max - eps_min) * eps_decay ** step)
     if np.random.rand() < eps:
-        return np.random.randint(n_output)
+        return np.random.randint(n_action)
     else:
         return mu([state])[0]
 
